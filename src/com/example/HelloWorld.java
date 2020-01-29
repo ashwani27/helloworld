a02ffc8ceee89698e2c6bd72dc3b1132490adbab
ashwanisharma <ashwanisharma@ashwani4141.Nagarro.local>
2020-01-30 01:17:22 +0530

    Test commit

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -3,5 +3,7 @@ package com.example;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("new branch sample");
+		//push
+		System.out.println("Git Push");
 	}
 }
